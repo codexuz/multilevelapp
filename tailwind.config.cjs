f3a7984a61870f373fbc063647d879452d05c92f
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-06-25 23:09:15 +0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,40 +4,12 @@ module.exports = {
 	'./node_modules/flowbite/**/*.js',     
 ],
 
- daisyui: {
-      themes: [
-        {
-          mytheme: {
-          
- "primary": "#570df8",
-          
- "secondary": "#f000b8",
-          
- "accent": "#1dcdbc",
-          
- "neutral": "#2b3440",
-          
- "base-100": "#ffffff",
-          
- "info": "#3abff8",
-          
- "success": "#36d399",
-          
- "warning": "#fbbd23",
-          
- "error": "#f87272",
-          },
-        },
-      ],
-    },
 
 darkMode: 'class', 
 	theme: {
 		extend: {},
 	},
 	plugins: [
-	    require('@tailwindcss/typography'),
-		require("daisyui"),
 		require('flowbite/plugin')
 	],
 }
