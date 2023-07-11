124b25bd91c92198a51afe2c74322ca2e53ce717
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-07-11 15:52:13 +0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 import dotenv from 'dotenv'; 
+import serviceWorker from "astrojs-service-worker";
+
 
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), serviceWorker()],
   publicDir: 'public',
   site: 'https://www.multiexam.vercel.app',
   cacheDir: './node_modules/.astro',
