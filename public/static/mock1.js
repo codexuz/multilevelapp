3a17d0f304a3c5f0f1226e1047f9b1a7712f0d20
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-07-27 14:43:56 +0500

    Update mock1.js

--- public/static/mock1.js
+++ public/static/mock1.js
@@ -23,7 +23,7 @@ ex0.classList.add("bg-red-100", "text-red-500","font-bold", "border-red-500")
 </svg>`;
 */
 ex0.classList.remove("bg-white", "border-blue-500")							
-ex0.classList.add("bg-green-100", "text-green-400","font-bold", "border-green-500")
+ex0.classList.add("bg-green-50", "text-green-400","font-bold", "border-green-500")
 score++;}
 }
 
@@ -38,7 +38,8 @@ part3=['commercially abroad', 'subscription channels', 'icky', 'more ambitious',
   <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
 </svg>`;
 */
-part03.classList.add("bg-red-100", "text-red-500","font-bold", "border-red-500")
+part03.classList.remove("bg-white", "border-blue-500")
+part03.classList.add("bg-red-50", "text-red-500","font-bold", "border-red-500")
 
 }
 						else {
@@ -46,7 +47,8 @@ part03.classList.add("bg-red-100", "text-red-500","font-bold", "border-red-500")
 							/*document.getElementById("s"+i).innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-green-400">
   <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
 </svg>`;
-*/	
+*/
+part03.classList.remove("bg-white", "border-blue-500")
 part03.classList.add("bg-green-100", "text-green-400","font-bold", "border-green-500")					
 score++;}
 					}
