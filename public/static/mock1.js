1295be781ba96dc622a1c560c31ce90371393c5d
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-07-29 13:59:35 +0500

    Update mock1.js

--- public/static/mock1.js
+++ public/static/mock1.js
@@ -29,8 +29,7 @@ score++;}
 
 //Part2
 
-for (var i=1; i<9; i++) 
-					{
+for (var i=7; i<15; i++) {
 						text3="sel"+i; text03="true"+i;
 					if (document.getElementById(text3).value=="1")	
 				{document.getElementById(text03).innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-green-400">
@@ -38,8 +37,8 @@ for (var i=1; i<9; i++)
 </svg>`;
 				  score++;}
 						
-					else { result=false; document.getElementById(text03).innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-green-400">
-  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+					else { result=false; document.getElementById(text03).innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-red-500">
+  <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd"></path>
 </svg>`;}
 					}
 
