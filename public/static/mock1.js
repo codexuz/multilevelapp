87df9f0f9080e7823eb6e3dd0016a9e400773c4d
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-07-29 15:36:15 +0500

    Update mock1.js

--- public/static/mock1.js
+++ public/static/mock1.js
@@ -24,7 +24,7 @@ ex0.classList.add("bg-pink-100", "text-pink-800","font-bold", "border-pink-800")
   <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
 </svg>`;
 */
-ex0.classList.add("bg-green-50", "text-green-800","font-bold", "border-green-800")
+ex0.classList.add("bg-green-50", "text-green-400","font-bold", "border-green-400")
 score++;}
 }
 
