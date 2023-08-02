d382ed39b63bb123ed3db507f392f3e29725bbd6
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-08-02 00:47:58 +0000

    "main"

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
